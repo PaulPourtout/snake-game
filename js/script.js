f5d8e75bcc134e5766ddbfb519565e8997e713b7
PaulPourtout <paulpourtout@hotmail.com>
2017-05-22 22:07:24 +0200

    eat apples make the snake longer and quicker

--- js/script.js
+++ js/script.js
@@ -3,106 +3,113 @@
 	const ctx = canvas.getContext('2d');
 
 	// object representing the game area as an array
-	// let area =
-	// 	[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-
-		const area = [];
-		for(let i = 0; i < 20; i++) {
-			area.push([]);
-			for(let y = 0; y < 20; y++) {
-				area[i].push(0);
-			}
+	const area = [];
+	for (let i = 0; i < 20; i++) {
+		area.push([]);
+		for (let y = 0; y < 20; y++) {
+			area[i].push(0);
 		}
-const areaBackgroundColor = "#FFFFFF";
-const startingPositionX = 3;
-const startingPositionY = 3;
+	}
+
+
+	const score = document.getElementById('score');
+	const areaBackgroundColor = "#FFFFFF";
+	const startingPositionX = 3;
+	const startingPositionY = 3;
+
+
+	// Apples' settings and methods
+	const apple = {
+		color: "#ea6b20",
+		width: 25,
+		positionX: null,
+		positionY: null,
+		getApplePosition: function () {
+			const applePositionX = Math.floor(Math.random() * (20));
+			const applePositionY = Math.floor(Math.random() * (20));
+			apple.positionX = applePositionX;
+			apple.positionY = applePositionY;
+		},
+		generateApple: function () {
+			area[apple.positionY][apple.positionX] = 2;
+		}
+	};
+
 
 	// Snake's settings and methods
 	const snake = {
 		apples: 0,
 		width: 25,
-		speed: 150,
-		length: 8,
+		speed: 200,
+		length: 3,
 		color: "#64b5f6",
 		presence: [],
 		positionX: startingPositionX,
 		positionY: startingPositionY,
 		setNewPosition: function () {
 			testGameOver();
+			eatApple();
 			area[snake.positionY][snake.positionX] = 1;
-			const newPosition = {x : snake.positionX, y: snake.positionY};
+			const newPosition = { x: snake.positionX, y: snake.positionY };
 			snake.presence.push(newPosition);
 
 			if (snake.presence.length > snake.length) {
 				const oldPosition = snake.presence.shift();
 				area[oldPosition.y][oldPosition.x] = 0;
 			}
-
 		},
 		goRight: function () {
 			snake.positionX++;
 			snake.setNewPosition();
-			// console.log("x", snake.positionX);
-			displaySnake();
+			displaySnakeAndApples();
 		},
 		goLeft: function () {
 			snake.positionX--;
 			snake.setNewPosition();
-			// console.log("x", snake.positionX);
-			displaySnake();
+			displaySnakeAndApples();
 		},
 		goDown: function () {
 			snake.positionY++;
 			snake.setNewPosition();
-			// console.log("y", snake.positionY);
-			displaySnake();
+			displaySnakeAndApples();
 		},
 		goUp: function () {
 			snake.positionY--;
 			snake.setNewPosition();
-			// console.log("y", snake.positionY);
-			displaySnake();
+			displaySnakeAndApples();
 		},
 	}
 
-// displaySnake function draw the snake in the canvas area with informations from the area array
-	let positionY;
-	let positionX;
+	// displaySnakeAndApples function draw the snake in the canvas area with informations from the area array
+	let snakePositionY;
+	let snakePositionX;
 
-	function displaySnake() {
+	function displaySnakeAndApples() {
 		area.forEach(function (element, index) {
-			positionY = snake.width * index;
+			snakePositionY = snake.width * index;
 
 			element.forEach(function (element, index, array) {
-				positionX = snake.width * index;
+				snakePositionX = snake.width * index;
 
 				if (element === 1) {
 					ctx.fillStyle = snake.color;
-					ctx.fillRect(positionX, positionY, snake.width, snake.width);
+					ctx.fillRect(snakePositionX, snakePositionY, snake.width, snake.width);
+				}
+
+				else if (element === 2) {
+					const applePositionX = snakePositionX + snake.width / 2;
+					const applePositionY = snakePositionY + snake.width / 2;
+
+					ctx.beginPath();
+					ctx.fillStyle = apple.color;
+					ctx.arc(applePositionX, applePositionY, snake.width / 2, 0, Math.PI * 2);
+					ctx.fill();
+					ctx.closePath();
 				}
 
 				else {
 					ctx.fillStyle = areaBackgroundColor;
-					ctx.fillRect(positionX, positionY, snake.width, snake.width);
+					ctx.fillRect(snakePositionX, snakePositionY, snake.width, snake.width);
 				}
 			});
 		});
@@ -111,17 +118,30 @@ const startingPositionY = 3;
 
 	// Test if the player lose the game
 	function testGameOver() {
-		snake.presence.forEach(function(element) {
-			if(snake.positionX === element.x
-			&& snake.positionY === element.y
-			&& element !== snake.presence[snake.presence.length-1]) {
+		snake.presence.forEach(function (element) {
+			if (snake.positionX === element.x
+				&& snake.positionY === element.y
+				&& element !== snake.presence[snake.presence.length - 1]) {
 				gameOver();
 			}
 		});
 
-		if (snake.positionX < 0 || snake.positionX > 20 
-		 || snake.positionY < 0 || snake.positionY > 20) {
+		if (snake.positionX < 0 || snake.positionX > 20
+			|| snake.positionY < 0 || snake.positionY > 20) {
 			gameOver();
+		}
+	}
+
+
+	// If the snake eat an apple increase snake's length and speed
+	function eatApple() {
+		if (apple.positionX === snake.positionX && apple.positionY === snake.positionY) {
+			snake.apples++;
+			score.innerHTML = snake.apples;
+			apple.getApplePosition();
+			apple.generateApple();
+			snake.speed = snake.speed - 5;
+			snake.length++;
 		}
 	}
 
@@ -132,15 +152,15 @@ const startingPositionY = 3;
 
 	// When the game is over is resets
 	function gameOver() {
-				alert('game over');
-			window.clearInterval(snakeUp);
-			window.clearInterval(snakeLeft);
-			window.clearInterval(snakeRight);
-			window.clearInterval(snakeDown);
+		alert('game over');
+		window.clearInterval(snakeUp);
+		window.clearInterval(snakeLeft);
+		window.clearInterval(snakeRight);
+		window.clearInterval(snakeDown);
 
-			location.reload();
-			snake.positionX = startingPositionX;
-			snake.positionY = startingPositionY;
+		location.reload();
+		snake.positionX = startingPositionX;
+		snake.positionY = startingPositionY;
 	}
 
 	// Change snake direction with arrow keys
@@ -178,5 +198,13 @@ const startingPositionY = 3;
 			snake.setNewPosition();
 		}
 	});
+
+
+
+	apple.getApplePosition();
+	apple.generateApple();
+	displaySnakeAndApples();
+
+
 
 })()
