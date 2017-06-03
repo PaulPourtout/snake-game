0a174e12bda8a75a7347d20616b230b7a1d92509
PaulPourtout <paulpourtout@hotmail.com>
2017-06-03 14:38:47 +0200

    Procfile and mongo connection added

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+// admin and mdp must go into variables
+mongoose.connect(`mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@ds159591.mlab.com:59591/snake-game`)
+
 const PORT = process.env.PORT || 8080;
 
 // Serve statics files (HTML, CSS, Js)
